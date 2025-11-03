fc2a281e3d9e279eb3ddce244c51469a7be4a4eb
Nhanthichcode <nhanlx151@gmail.com>
2025-11-03 16:51:43 +0700

    fix reply-comment

--- obj/Debug/net8.0/Trang tin điện tử mvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/Trang tin điện tử mvc.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trang tin điện tử mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210a45b163905af4fa94743661ab24cb328db7ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea6303d43cc40d4e5a7d4aa1976ff262268472cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trang tin điện tử mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trang tin điện tử mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
