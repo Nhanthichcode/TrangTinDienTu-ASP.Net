64805eb5eee9f75ece703d996af445d567860e0f
Nhanthichcode <nhanlx151@gmail.com>
2025-11-18 18:05:49 +0700

    fea: thêm bảng cho trang chủ

---
+++ Views/Home/_ArticleListPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Trang_tin_điện_tử_mvc.Views.Home
+{
+    public class _ArticleListPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
