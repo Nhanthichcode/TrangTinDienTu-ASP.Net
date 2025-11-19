7c1ad84d6acd8cb42749acfc9ebe455c67bbfb3c
Nhanthichcode <nhanlx151@gmail.com>
2025-11-19 21:37:55 +0700

    fea: quản lý ảnh trong bài viết

---
+++ Models/ArticleImagePosition.cs
@@ -0,0 +1,14 @@
+﻿namespace Trang_tin_điện_tử_mvc.Models
+{
+    public class ArticleImagePosition
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+        public int MediaId { get; set; }
+        public int PositionIndex { get; set; } // Thứ tự ảnh trong content
+        public string Placeholder { get; set; } // Chuỗi thay thế, ví dụ: "{image0}"
+
+        public Article Article { get; set; }
+        public Media Media { get; set; }
+    }
+}
