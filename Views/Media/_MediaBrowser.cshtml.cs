c5ef20d25b24e28f2eb62a01acfe3e74e6187948
Nhanthichcode <nhanlx151@gmail.com>
2025-11-19 21:36:31 +0700

    fea: thêm quản lý ảnh trong bài viết

---
+++ Views/Media/_MediaBrowser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Trang_tin_điện_tử_mvc.Views.Media
+{
+    public class _MediaBrowserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
