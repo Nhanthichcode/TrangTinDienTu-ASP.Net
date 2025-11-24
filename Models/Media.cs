991e9e1a49d28a409a4876c7ffd7c8b8ee8cfd5d
Nhanthichcode <nhanlx151@gmail.com>
2025-11-24 21:03:19 +0700

    fea: thêm trường để quản lý

--- Models/Media.cs
+++ Models/Media.cs
@@ -1,16 +1,27 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Trang_tin_điện_tử_mvc.Models
 {
-        public class Media
-    {
+    public class Media
+    {      
         [DisplayName("Mã Media")]
         public int Id { get; set; }
 
         [DisplayName("Mã bài viết")]
         public int? ArticleId { get; set; }
         public Article? Article { get; set; }
+        
+        [DisplayName("Phân loại")]
+        public string Category { get; set; } = "Content";
+        // Các giá trị dự kiến: "UserAvatar", "ArticleThumbnail", "ArticleContent"
+
+        [DisplayName("Người tải lên")]
+        public string? UploadedByUserId { get; set; }
+
+        [ForeignKey("UploadedByUserId")]
+        public ApplicationUser? UploadedByUser { get; set; }
 
         [DisplayName("Tên file")]
         [Required]
--- Models/MediaIndexViewModel.cs
+++ Models/MediaIndexViewModel.cs
@@ -10,12 +10,14 @@
         public int UsedMediaCount { get; set; }
         public int UnusedMediaCount { get; set; }
         public long TotalSizeKB { get; set; } // Dùng long để tránh tràn số nếu dung lượng lớn
-
-        // Các thông tin hỗ trợ cho View (dropdown, phân trang)
+        public string CurrentStatus { get; set; } // "used" hoặc "unused"
+        public Microsoft.AspNetCore.Mvc.Rendering.SelectList StatusDropdown { get; set; }
         public Microsoft.AspNetCore.Mvc.Rendering.SelectList ArticlesDropdown { get; set; }
+        public Microsoft.AspNetCore.Mvc.Rendering.SelectList CategoriesDropdown { get; set; }
         public int CurrentPage { get; set; }
         public int TotalPages { get; set; }
         public string CurrentSearch { get; set; }
         public int? CurrentArticleId { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
